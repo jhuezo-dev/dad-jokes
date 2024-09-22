684014cf2db94eae33fcde6287ae4a8f4be64ab3
jhuezo-dev <jessicahuezodev@gmail.com>
2024-09-21 23:26:26 -0700

    added state management

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
-import './css/main.css'
+import './css/main.scss'
 import App from './App.vue'
 import router from './routes'
+import { pinia } from './stores'
 
 const app = createApp(App); 
 
+app.use(pinia);
 app.use(router);
 app.mount('#app');
