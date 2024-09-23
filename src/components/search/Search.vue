c3083761202eb90463e10e30ca2c1deeed144f01
jhuezo-dev <jessicahuezodev@gmail.com>
2024-09-23 10:56:02 -0700

    adding pagination to search component

--- src/components/search/Search.vue
+++ src/components/search/Search.vue
@@ -46,48 +46,66 @@
                     </li>
                   </ul>
                 </div>
-                
 
 
 
-                <div v-if="pageData" class="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-  <div class="flex flex-1 justify-between sm:hidden">
-    <a href="#" class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Previous</a>
-    <a href="#" class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Next</a>
-  </div>
-  <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-    <div>
-      <p class="text-sm text-gray-700">
-        Showing
-        <span class="font-medium">{{ pageData.current_page }}</span>
-        to
-        <span class="font-medium">{{ pageData.limit }}</span>
-        of
-        <span class="font-medium">{{ pageData.total_pages }}</span>
-        results
-      </p>
-    </div>
-    <div>
-      <nav class="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
-        <a href="#" class="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
-          <span class="sr-only">Previous</span>
-          <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-            <path fill-rule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clip-rule="evenodd" />
-          </svg>
-        </a>
-        <!-- Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" -->
-        <a href="#" aria-current="page" class="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">1</a>
-        
-        <a href="#" class="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
-          <span class="sr-only">Next</span>
-          <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-            <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
-          </svg>
-        </a>
-      </nav>
-    </div>
-  </div>
-</div>
+
+                <div v-if="pageData"
+                  class="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+                  <div class="flex flex-1 sm:hidden">
+                    <button
+                    v-if="pageData.current_page !== pageData.previous_page"
+                      class="button mr-auto"
+                      :class="`${pageData.current_page === pageData.previous_page ? 'hover:bg-gray-50 focus:z-20 focus:outline-offset-0' : ''}`"
+                      :disabled="pageData.current_page === pageData.previous_page">Previous</button>
+                    <button v-if="pageData.next_page < pageData.total_pages"
+                      class="button ml-auto">Next</button>
+                  </div>
+                  <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+                    <div>
+                      <p class="text-sm text-gray-700">
+                        Showing
+                        <span class="font-medium">{{ pageData.current_page }}</span>
+                        to
+                        <span class="font-medium">{{ pageData.limit }}</span>
+                        of
+                        <span class="font-medium">{{ pageData.total_pages }}</span>
+                        results
+                      </p>
+                    </div>
+                    <div>
+                      <nav class="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
+                        <button
+                        v-if="pageData.current_page !== pageData.previous_page"
+                          class="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 "
+                          :class="`${pageData.current_page === pageData.previous_page ? 'hover:bg-gray-50 focus:z-20 focus:outline-offset-0' : ''}`"
+                          :disabled="pageData.current_page === pageData.previous_page">
+                          <span class="sr-only">Previous</span>
+                          <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <path fill-rule="evenodd"
+                              d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
+                              clip-rule="evenodd" />
+                          </svg>
+                        </button>
+                        <!-- Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" -->
+                        
+                        <div id="pagination_numbers" aria-current="page"
+                          class="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">{{ p }}</div>
+                        
+
+                        <button
+                          class="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0" v-if="pageData.next_page < pageData.total_pages">
+                          <span class="sr-only">Next</span>
+                          <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <path fill-rule="evenodd"
+                              d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+                              clip-rule="evenodd" />
+                          </svg>
+                        </button>
+                      </nav>
+                    </div>
+                  </div>
+                </div>
 
 
 
@@ -110,18 +128,51 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import { useSearchStore } from '@/stores/search';
 import { getAll } from '@/api';
+
 
 const jokes = ref([]);
 const searchStore = useSearchStore();
 const pageData = ref();
 // console.log('modal open', searchStore.modal_open)
 
+const paginationLimit = ref(20);
+const pageCount = ref();
+const currPageNumber = ref(1);
+
+const startIndex = computed(() => {
+  return currPageNumber.value - 1 * paginationLimit.value
+})
+
+const endIndex = computed(() => {
+  return startIndex.value + paginationLimit.value
+})
+
+const numberOfPages = computed(() => {
+  return Math.ceil(jokes.value.length / paginationLimit.value)
+})
+
+const paginatedData = computed(() => {
+  return jokes.value.slice(startIndex.value, endIndex.value)
+})
+
 onMounted(async () => {
   await getJokes();
 })
+
+function nextPage() {
+  if(currPageNumber.value < numberOfPages.value) {
+    currPageNumber.value += 1;
+  }
+}
+
+function prevPage() {
+  if(currPageNumber.value > 1) {
+    currPageNumber.value -= 1;
+  }
+}
 
 async function getJokes() {
   const data = await getAll();
@@ -130,9 +181,12 @@ async function getJokes() {
     jokes.value = []
   }
   pageData.value = data
+  pageCount.value = Math.ceil(pageData.value.limit / paginationLimit.value)
+
   jokes.value = data.results
   console.log('all jokes', jokes.value)
   console.log('pageData', pageData.value)
+  console.log('pageCount', pageCount.value)
 }
 
 function ToggleModalOpen() {
