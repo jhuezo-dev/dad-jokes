4e157d44d7ef3d0e793e9f57aa6b2c5fe8da3d4c
Jessica Huezo <jessicabhuezo@gmail.com>
2024-09-24 00:32:35 -0700

    adds link to view single joke

--- src/views/Jokes.vue
+++ src/views/Jokes.vue
@@ -3,7 +3,11 @@
     <div v-if="jokes.length > 0">
       <h1 class="justify-start font-medium">Enjoy these again</h1>
       <div class="text-xl my-6">View a random joke <router-link to="/">here</router-link></div>
-      <div class="text-xl my-6" v-for="j of jokes" :key="j.id">{{ j.joke }}</div>
+      <div class="text-xl my-6" v-for="j of jokes" :key="j.id">
+        <router-link :to="`/jokes/${j.id}`">
+          {{ j.joke }}
+        </router-link>
+      </div>
     </div>
     <div class="justify-start font-medium" v-else>
       <h1>Search up some jokes then you'll see them here!</h1>
@@ -19,7 +23,6 @@ import { usecachedJokesStore } from '@/stores/cachedJokes';
 const cachedJokesStore = usecachedJokesStore();
 const jokes = ref([]);
 onMounted(async() => {
-  console.log('stored jokes', cachedJokesStore.jokes)
   if(cachedJokesStore.jokes.length > 0) {
     jokes.value = cachedJokesStore.jokes;
   }
