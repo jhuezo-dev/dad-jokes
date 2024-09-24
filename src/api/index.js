f7029fae3f0596f67b96d50328b69f72d44e7db7
Jessica Huezo <jessicabhuezo@gmail.com>
2024-09-24 04:11:17 -0700

    extracted out term search to single function call

--- src/api/index.js
+++ src/api/index.js
@@ -3,13 +3,12 @@ const headers = new Headers({
   'Accept': 'application/json'
 });
 
-export async function getAll(page, limit, term) {
+export async function getAll(page, limit) {
   let url = 'https://icanhazdadjoke.com/search';
   let params = new URLSearchParams();
 
   if(page) params.append('page',page)
   if(limit) params.append('limit',limit)
-  if(term) params.append('term', term)
   if(params.size) {
     url += '?' + params;
   }
@@ -18,6 +17,28 @@ export async function getAll(page, limit, term) {
       const response = await fetch(url, {headers: headers});
       if(!response.ok) {
         throw new Error(`Error retrieving random joke: ${response.status}`);
+      }
+      const data = await response.json();
+      resolve(data);
+    } catch (error) {
+      reject(error.message);
+    }
+  });
+}
+
+export async function getTerm(term) {
+  let url = 'https://icanhazdadjoke.com/search';
+  let params = new URLSearchParams();
+
+  if(term) params.append('term', term)
+  if(params.size) {
+    url += '?' + params;
+  }
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await fetch(url, {headers: headers});
+      if(!response.ok) {
+        throw new Error(`Error retrieving term: ${response.status}`);
       }
       const data = await response.json();
       resolve(data);
