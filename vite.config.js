61e3e0f01759d0e22f708d0fe860142f4987ef4f
Jessica Huezo <jessicabhuezo@gmail.com>
2024-09-24 01:55:00 -0700

    reverted github pages updates

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,5 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  assetsInclude: ["*.png", "*.avif"],
-  base: '/<REPO>/'
+  assetsInclude: ["*.png", "*.avif"]
 })
